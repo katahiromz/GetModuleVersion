fac6475147389123b7fd51bca25eab67418ae366
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-06-19 06:52:14 +0900

    improve 2

--- GetModuleVersion.c
+++ GetModuleVersion.c
@@ -11,6 +11,17 @@ typedef struct tagLANGANDCODEPAGE
     WORD wCodePage;
 } LANGANDCODEPAGE, *PLANGANDCODEPAGE;
 
+// English (US) UTF-16
+#define PRODUCT_VER_ENGLISH_US_UTF16    "\\StringFileInfo\\040904E4\\ProductVersion"
+// German (Germany) UTF-16
+#define PRODUCT_VER_GERMAN_UTF16        "\\StringFileInfo\\040704E4\\ProductVersion"
+// English (US) Western European
+#define PRODUCT_VER_ENGLISH_US_WE       "\\StringFileInfo\\040904B0\\ProductVersion"
+// English (US) Neutral
+#define PRODUCT_VER_ENGLISH_US_NEUTRAL  "\\StringFileInfo\\04090000\\ProductVersion"
+// Swedish (Sweden) Western European
+#define PRODUCT_VER_SWEDISH_WE          "\\StringFileInfo\\041D04B0\\ProductVersion"
+
 HRESULT GetModuleVersion(PCSTR pszFileName, PSTR *ppszDest)
 {
     DWORD dwHandle;
@@ -34,6 +45,16 @@ HRESULT GetModuleVersion(PCSTR pszFileName, PSTR *ppszDest)
     GetFileVersionInfoA(pszFileName, dwHandle, size, pbData);
 
     HRESULT hr = E_FAIL;
+    if ((VerQueryValueA(pbData, PRODUCT_VER_ENGLISH_US_UTF16,   &pvData, &size) ||
+         VerQueryValueA(pbData, PRODUCT_VER_GERMAN_UTF16,       &pvData, &size) ||
+         VerQueryValueA(pbData, PRODUCT_VER_ENGLISH_US_WE,      &pvData, &size) ||
+         VerQueryValueA(pbData, PRODUCT_VER_ENGLISH_US_NEUTRAL, &pvData, &size) ||
+         VerQueryValueA(pbData, PRODUCT_VER_SWEDISH_WE,         &pvData, &size)) && size)
+    {
+        // NOTE: *ppszDest must be freed using LocalFree later
+        *ppszDest = StrDupA((PSTR)pvData);
+        hr = *ppszDest ? S_OK : E_OUTOFMEMORY;
+    }
     if (VerQueryValueA(pbData, "\\VarFileInfo\\Translation", &pvData, &size))
     {
         PVOID pDataSaved = pvData;
@@ -47,7 +68,7 @@ HRESULT GetModuleVersion(PCSTR pszFileName, PSTR *ppszDest)
             {
                 // NOTE: *ppszDest must be freed using LocalFree later
                 *ppszDest = StrDupA((PSTR)pvData);
-                hr = S_OK;
+                hr = *ppszDest ? S_OK : E_OUTOFMEMORY;
             }
         }
     }
